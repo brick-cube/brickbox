934c8b6cf86f7b700edb33e960a8e57af37cfd0e
Melbin Mathew <melbinppmathewp@gmail.com>
2025-11-08 21:38:57 +0530

    2nd commit - Added local project files

---
+++ app/Filament/Resources/Companies/Schemas/CompanyInfolist.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Companies\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class CompanyInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('name'),
+                TextEntry::make('email')
+                    ->label('Email address')
+                    ->placeholder('-'),
+                TextEntry::make('phone')
+                    ->placeholder('-'),
+                TextEntry::make('address')
+                    ->placeholder('-'),
+                TextEntry::make('subscription_status'),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+            ]);
+    }
+}
